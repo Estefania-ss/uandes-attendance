b252546957101912b5bfbe5c4a09c310ac85c6e9
daniel9091 <djguzman1@miuandes.cl>
2025-07-06 20:02:09 -0400

    login azul

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  layout :layout_by_resource
 
   private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 
   # Métodos helper para verificar permisos
   def require_admision!
